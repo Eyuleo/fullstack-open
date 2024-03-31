9106e4b0cfe4c676ee128cd28f81f833a8aac021
Eyuleo <eyuleo10@gmail.com>
2024-03-31 15:34:48 +0300

    add name filtering

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-	const [persons, setPersons] = useState([])
+	const [persons, setPersons] = useState([
+		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+	])
 	const [newName, setNewName] = useState('')
 	const [phoneNumber, setPhoneNumber] = useState('')
+	const [query, setQuery] = useState('')
 
 	const handleNameChange = (event) => {
 		setNewName(event.target.value)
@@ -23,16 +29,32 @@ const App = () => {
 		if (isDuplicate) {
 			alert(`${newName} is already added to phonebook`)
 			return
+		} else {
+			setPersons([
+				...persons,
+				{ name: newName, number: phoneNumber, id: persons.length + 1 },
+			])
+			setNewName('')
+			setPhoneNumber('')
 		}
-		setPersons([...persons, { name: newName, number: phoneNumber }])
-		setNewName('')
-		setPhoneNumber('')
 	}
+
+	const filteredContacts = persons.filter((person) => {
+		return person.name.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+	})
 
 	return (
 		<div>
-			<h2>Phonebook</h2>
+			<h1>Phonebook</h1>
+			<label htmlFor="search">filter</label>
+			<input
+				type="text"
+				id="search"
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+			/>
 			<form onSubmit={addNewContact}>
+				<h2>add a new</h2>
 				<div>
 					<label htmlFor="contact">name:</label>
 					<input id="contact" value={newName} onChange={handleNameChange} />
@@ -46,8 +68,8 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map((person) => (
-				<p key={person.name}>
+			{filteredContacts.map((person) => (
+				<p key={person.id}>
 					{person.name} {person.number}
 				</p>
 			))}
